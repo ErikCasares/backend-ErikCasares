0ad533ead5649ba8783c6375f1bcaf9a5c772c1b
Erik Casares <erik.casaresps@gmail.com>
2024-02-10 15:19:52 -0300

    reentrega

--- index.js
+++ index.js
@@ -31,21 +31,30 @@ class ProductManager {
         this.idCounter = this.products.length + 1;
         this.PATH = "./products.json"
     }
-    getProduct(PATH) {
+    getProduct() {
         try {
-            // Lee el contenido del archivo products.json
-            const data = fs.readFileSync(PATH, 'utf8');
-
-            // Parsea el contenido a un objeto JavaScript
-            this.products = JSON.parse(data);
-
-            console.log('Productos cargados:', this.products);
-
-            return this.products;
+            //comprueba que exista el archivo
+            if (fs.existsSync(this.PATH)) {
+                // Lee el contenido del archivo.json
+                const data = fs.readFileSync(this.PATH, 'utf8');
+    
+                // Parsea el contenido a un objeto javascript
+                this.products = JSON.parse(data);
+    
+                console.log('Productos cargados:', this.products);
+    
+                return this.products;
+            } else {
+                // si el archivo no existe, lo crea con un array vacio 
+                console.log('No existe el archivo de productos. Inicializando con un array vacío.');
+                fs.writeFileSync(this.PATH, JSON.stringify(this.products), 'utf8');
+                console.log(this.products)
+                return this.products;
+            }
         } catch (error) {
-            // Si hay un error (por ejemplo, el archivo no existe), devuelve un array vacío
-            console.error('Error al cargar productos:', error.message);
-            return [];
+            // Log any errors that occur during file read/write
+            console.error('Error al cargar o inicializar productos:', error);
+            throw error; // Optionally, you can choose to throw the error here to indicate failure.
         }
     }
 
@@ -65,6 +74,7 @@ class ProductManager {
         newProduct.id = this.idCounter++;
         //Agregar producto al arreglo
         this.products.push(newProduct);
+        this.saveProductsToFile()
         console.log(`Producto agregado: ${newProduct.title} (ID: ${newProduct.id})`)
 
     }
@@ -78,6 +88,7 @@ class ProductManager {
                 ...updatedFields
             };
             const productoactualizado = this.products[productIndex]
+            this.saveProductsToFile()
             console.log(`Producto actualizado: { ID: ${productoactualizado.id}, Código: ${productoactualizado.code}, Nombre: ${productoactualizado.title} }`);
 
         } else {
@@ -90,6 +101,7 @@ class ProductManager {
 
         if (productIndex !== -1) {
             const deletedProduct = this.products.splice(productIndex, 1)[0];
+            this.saveProductsToFile()
             console.log(`Producto eliminado: { ID: ${deletedProduct.id}, Código: ${deletedProduct.code}, Nombre: ${deletedProduct.title} }`);
         } else {
             console.log(`No se encontró ningún producto con el ID ${id}.`);
@@ -110,15 +122,15 @@ class ProductManager {
         return product;
     }
 
-    saveProductsToFile(PATH) {
+    saveProductsToFile() {
         return new Promise((resolve, reject) => {
             const productsJSON = JSON.stringify(this.products, null, 2);
 
-            fs.writeFileSync(PATH, productsJSON, 'utf8', (err) => {
+            fs.writeFileSync(this.PATH, productsJSON, 'utf8', (err) => {
                 if (err) {
                     reject(err);
                 } else {
-                    resolve(`Productos guardados en ${PATH}`);
+                    resolve(`Productos guardados en ${this.PATH}`);
                 }
             });
         });
@@ -128,6 +140,7 @@ class ProductManager {
 //Uso
 const manager = new ProductManager();
 
+manager.getProduct();
 //agregando muchos productos sin repetir
 
 manager.addProduct("papa", "una simple papa", 10, "foto-de-una-papa,jpg", "P001", 7);
@@ -176,7 +189,7 @@ manager.addProduct("Producto 10", "Descripción 3", 29, "img10.jpg", "P010", 39)
 
 // actualizando un json de items
 
-manager.saveProductsToFile('products.json')
+manager.saveProductsToFile()
     .then((message) => {
         console.log(message);
     })
@@ -185,14 +198,14 @@ manager.saveProductsToFile('products.json')
     });
 
 //leyendo productos desde el json 
-manager.getProduct('products.json')
+manager.getProduct()
 
 //agregando despues del json
 
 manager.addProduct("Producto 11", "agregado despues del json", 29, "img10.jpg", "P011", 39);
 
 //volviendo a guardar el json
-manager.saveProductsToFile('products.json')
+manager.saveProductsToFile()
     .then((message) => {
         console.log(message);
     })
